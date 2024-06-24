00182e580256b935d3b8658ab9f257f189a5d51a
vitorjzzz <vitorjosexadrez@gmail.com>
2024-06-24 20:34:42 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+        autoplay:true
+    });
+
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle();
+    })
+    
+    $('#telefone').mask('(00) 00000-0000') //nesses plugins o "0" representa número e o S representa letras
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true,
+            },
+            veiculoDeInteresse: {
+                required: false,
+            }
+        },
+        messages: {
+            nome: "Por favor, Insira seu Nome"
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos!`)
+            }
+        }
+     })
+    
+     $('.lista-veiculos button').click(function(){
+        const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
+
+        $('#veiculoDeInteresse').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+     })
+})
